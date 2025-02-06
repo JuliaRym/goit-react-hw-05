5a5124972f8e760e3ac70057bb6591cd91e240b1
JuliaRym <167142301+JuliaRym@users.noreply.github.com>
2025-02-07 00:16:34 +0100

    Update MovieCast.jsx

--- src/components/MovieCast.jsx
+++ src/components/MovieCast.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { getMovieCast } from "../api/moviesApi";
+import { getMovieCast } from "../api/MoviesApi";
 
 const MovieCast = () => {
   const { movieId } = useParams();
